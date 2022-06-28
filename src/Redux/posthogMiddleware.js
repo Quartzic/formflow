a4e96221d4602e93689bae07454bba17c935b7ee
Nathan Goller-Deitsch <nathan@quartzic.co>
2022-06-28 11:31:54 -0400

    disable middleware when running unit tests

--- src/Redux/posthogMiddleware.js
+++ src/Redux/posthogMiddleware.js
@@ -1,8 +1,11 @@
 import posthog from 'posthog-js';
 
 const posthogMiddleware = storeAPI => next => action => {
-    posthog.capture("Redux event dispatched", {type: action.type});
-    next(action)
+    // disable middleware when running unit tests
+    if(process.env.NODE_ENV !== 'test') {
+        posthog.capture("Redux event dispatched", {type: action.type});
+        next(action)
+    }
 }
 
 export default posthogMiddleware;
