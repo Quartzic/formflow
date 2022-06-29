f85f30deafaa9c5e8ae0cb41d0d7fabf588d4777
Nathan Goller-Deitsch <nathan@quartzic.co>
2022-06-29 15:34:17 -0400

    fix unit tests

--- src/Redux/reducers.test.js
+++
@@ -1,7 +0,0 @@
-import {reducers} from "./store";
-
-test('reducers', () => {
-    let state = reducers(undefined, {type: '@@INIT'});
-    state = reducers(state, {type:'metadata/set',payload:{username:'JohnDoe123',refNumber:'453459012',workflow:'validation',timestamp:'2022-06-21T17:24:19.020Z'}});
-    expect(state).toEqual({submissions:[],barcodes:[],workflow:null,metadata:{username:'JohnDoe123',refNumber:'453459012',workflow:'validation',timestamp:'2022-06-21T17:24:19.020Z'}});
-});
